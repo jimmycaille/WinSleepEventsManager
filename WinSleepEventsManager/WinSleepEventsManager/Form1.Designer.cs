862d9bc9142189f8c3cd53d132650305f1f12a2a
Jimmy.Caille <jimmy.caille@gmail.com>
2019-01-30 12:38:51 +0100

    first commit

---
+++ WinSleepEventsManager/WinSleepEventsManager/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace WinSleepEventsManager {
+    partial class Form1 {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 184);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(532, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "xx / xx";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(66, 15);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(159, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "ID 1 : Power-Troubleshooter";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(66, 38);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(124, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "ID 42 : Kernel-Power";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(66, 62);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(191, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "ID 1001 : Windows Error Reporting";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(66, 86);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(119, 17);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "ID 6005 : EventLog";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Checked = true;
+            this.checkBox5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox5.Location = new System.Drawing.Point(66, 110);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(119, 17);
+            this.checkBox5.TabIndex = 6;
+            this.checkBox5.Text = "ID 6006 : EventLog";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Checked = true;
+            this.checkBox6.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox6.Location = new System.Drawing.Point(66, 134);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(119, 17);
+            this.checkBox6.TabIndex = 7;
+            this.checkBox6.Text = "ID 6008 : EventLog";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(337, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(512, 285);
+            this.textBox1.TabIndex = 8;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(303, 247);
+            this.tabControl1.TabIndex = 9;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.checkBox8);
+            this.tabPage1.Controls.Add(this.checkBox7);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(295, 221);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Application";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Checked = true;
+            this.checkBox8.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox8.Location = new System.Drawing.Point(90, 53);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(115, 17);
+            this.checkBox8.TabIndex = 1;
+            this.checkBox8.Text = "ID 26212 : Chkdsk";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Checked = true;
+            this.checkBox7.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox7.Location = new System.Drawing.Point(90, 29);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(105, 17);
+            this.checkBox7.TabIndex = 0;
+            this.checkBox7.Text = "ID 1001 : Wininit";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.checkBox10);
+            this.tabPage2.Controls.Add(this.checkBox9);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(295, 221);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Security";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(99, 145);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Checked = true;
+            this.checkBox10.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox10.Location = new System.Drawing.Point(88, 65);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(111, 17);
+            this.checkBox10.TabIndex = 1;
+            this.checkBox10.Text = "ID 4801 : Auditing";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Checked = true;
+            this.checkBox9.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox9.Location = new System.Drawing.Point(88, 41);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(111, 17);
+            this.checkBox9.TabIndex = 0;
+            this.checkBox9.Text = "ID 4800 : Auditing";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(295, 221);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Setup";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.checkBox1);
+            this.tabPage4.Controls.Add(this.checkBox3);
+            this.tabPage4.Controls.Add(this.checkBox2);
+            this.tabPage4.Controls.Add(this.checkBox5);
+            this.tabPage4.Controls.Add(this.checkBox6);
+            this.tabPage4.Controls.Add(this.button1);
+            this.tabPage4.Controls.Add(this.checkBox4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(295, 221);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "System";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(295, 221);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "ForwardedEvents";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(337, 3);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(132, 17);
+            this.checkBox11.TabIndex = 10;
+            this.checkBox11.Text = "Reverse chronological";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 324);
+            this.Controls.Add(this.checkBox11);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox11;
+    }
+}
+
---
+++ WinSleepEventsManager/WinSleepEventsManager/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WinSleepEventsManager {
+    public partial class Form1 : Form {
+        private const String separationString = "####################################################################\r\n";
+        private Boolean continueReadSystem;
+        private Boolean continueReadApp;
+        private Boolean continueReadSecurity;
+        public Form1() {
+            InitializeComponent();
+            continueReadSystem   = false;
+            continueReadApp      = false;
+            continueReadSecurity = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (continueReadSystem) {
+                button1.Text = "Start";
+                continueReadSystem = false;
+            } else {
+                button1.Text = "Stop";
+                continueReadSystem = true;
+                Thread thread = new Thread(new ThreadStart(readSystemThread));
+                thread.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            if (continueReadApp) {
+                button2.Text = "Start";
+                continueReadApp = false;
+            } else {
+                button2.Text = "Stop";
+                continueReadApp = true;
+                Thread thread = new Thread(new ThreadStart(readAppThread));
+                thread.Start();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            //check if admin
+            WindowsPrincipal principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool isElevated = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!isElevated) {
+                MessageBox.Show(this, "Please restart the app with admin rights !", "Needs elevation !", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                ProcessStartInfo elevated = new ProcessStartInfo(System.Reflection.Assembly.GetEntryAssembly().Location, "run2");
+                elevated.UseShellExecute = true;
+                elevated.Verb = "runas";
+                try {
+                    Process.Start(elevated);
+                    Application.Exit();
+                } catch (Exception ex) {
+                    MessageBox.Show(this, "You dismissed the prompt...", "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                }
+            } else {
+                if (continueReadSecurity) {
+                    button3.Text = "Start";
+                    continueReadSecurity = false;
+                } else {
+                    button3.Text = "Stop";
+                    continueReadSecurity = true;
+                    Thread thread = new Thread(new ThreadStart(readSecurityThread));
+                    thread.Start();
+                }
+            }
+        }
+        public void readSystemThread() {
+            this.Invoke((MethodInvoker)delegate {
+                textBox1.Clear();
+            });
+            string eventLogName = "System";
+
+            EventLog eventLog = new EventLog();
+            eventLog.Log = eventLogName;
+
+            int i = 1;
+
+            if (checkBox11.Checked) {
+                //TODO
+
+            }
+            //eventLog.Entries.Cast<EventLogEntry>().Reverse()
+            foreach (EventLogEntry log in eventLog.Entries) {
+                if (!continueReadSystem)
+                    break;
+                this.Invoke((MethodInvoker)delegate {
+                    // Running on the UI thread
+                    label1.Text = i + " / " + eventLog.Entries.Count;
+                });
+                //Out of sleep
+                if (checkBox1.Checked && log.EventID == 1 && log.Source.Contains("Power-Troubleshooter")) {
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text += String.Format("{0} Exits sleep, reason : {1}\r\n", log.TimeGenerated, "asf");
+                    });
+                }
+                //Enter sleep
+                if (checkBox2.Checked && log.EventID == 42 && log.Source.Contains("Kernel-Power")) {
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text += String.Format("{0} Enter sleep\r\n", log.TimeGenerated);
+                    });
+                }
+                //Crash/BSOD   Windows Error Reporting ?? BugCheck ok
+                if (checkBox3.Checked && log.EventID == 1001 && log.Source.Contains("BugCheck")) {
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text += separationString;
+                        textBox1.Text += String.Format("{0} Crash/BSOD : \r\n{1}\r\n\r\n", log.TimeGenerated, log.Message);
+                    });
+                }
+                //Service started
+                if (checkBox4.Checked && log.EventID == 6005 && log.Source.Contains("EventLog")) {
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text += String.Format("{0} EventVwr started (startup)\r\n", log.TimeGenerated);
+                    });
+                }
+                //Service stopped
+                if (checkBox5.Checked && log.EventID == 6006 && log.Source.Contains("EventLog")) {
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text += String.Format("{0} EventVwr stopped (shutdown)\r\n", log.TimeGenerated);
+                    });
+                }
+                //Forced shutdown
+                if (checkBox6.Checked && log.EventID == 6008 && log.Source.Contains("EventLog")) {
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text += String.Format("{0} Forced shutdown\r\n", log.TimeGenerated);
+                    });
+                }
+                i++;
+                if (i >= 40000)
+                    break;
+            }
+            continueReadSystem = false;
+            this.Invoke((MethodInvoker)delegate {
+                button1.Text = "Start";
+            });
+        }
+        public void readAppThread() {
+            this.Invoke((MethodInvoker)delegate {
+                textBox1.Clear();
+            });
+            string eventLogName = "Application";
+
+            EventLog eventLog = new EventLog();
+            eventLog.Log = eventLogName;
+
+            int i = 1;
+            //eventLog.Entries.Cast<EventLogEntry>().Reverse()
+            foreach (EventLogEntry log in eventLog.Entries) {
+                if (!continueReadApp)
+                    break;
+                this.Invoke((MethodInvoker)delegate {
+                    // Running on the UI thread
+                    label1.Text = i + " / " + eventLog.Entries.Count;
+                });
+                //chkdsk during boot
+                if (checkBox7.Checked && log.EventID == 1001 && log.Source.Contains("Wininit")) {
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text += separationString;
+                        textBox1.Text += String.Format("{0} Chkdsk during boot :\r\n{1}\r\n\r\n", log.TimeGenerated, log.Message);
+                    });
+                }
+                //chkdsk during session 26212? 26214 ok
+                if (log.Source.Contains("Chkdsk")) {
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text += separationString;
+                        textBox1.Text += String.Format("{0} Chkdsk during session :\r\n{1}\r\n\r\n", log.TimeGenerated, log.Message);
+                    });
+                }
+                i++;
+                if (i >= 40000)
+                    break;
+            }
+            continueReadApp = false;
+            this.Invoke((MethodInvoker)delegate {
+                button2.Text = "Start";
+            });
+        }
+        public void readSecurityThread() {
+            this.Invoke((MethodInvoker)delegate {
+                textBox1.Clear();
+            });
+            string eventLogName = "Security";
+
+            EventLog eventLog = new EventLog();
+            eventLog.Log = eventLogName;
+
+            int i = 1;
+            //eventLog.Entries.Cast<EventLogEntry>().Reverse()
+            foreach (EventLogEntry log in eventLog.Entries) {
+                if (!continueReadSecurity)
+                    break;
+                this.Invoke((MethodInvoker)delegate {
+                    // Running on the UI thread
+                    label1.Text = i + " / " + eventLog.Entries.Count;
+                });
+                //Windows locked
+                if (checkBox9.Checked && log.EventID == 4800 && log.Source.Contains("Auditing")) {
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text += String.Format("{0} Windows locked\r\n", log.TimeGenerated);
+                    });
+                }
+                //Windos unlocked
+                if (checkBox10.Checked && log.EventID == 4801 && log.Source.Contains("Auditing")) {
+                    this.Invoke((MethodInvoker)delegate {
+                        textBox1.Text += String.Format("{0} Windows unlocked\r\n", log.TimeGenerated);
+                    });
+                }
+                i++;
+                if (i >= 40000)
+                    break;
+            }
+            continueReadSecurity = false;
+            this.Invoke((MethodInvoker)delegate {
+                button3.Text = "Start";
+            });
+        }
+    }
+}
